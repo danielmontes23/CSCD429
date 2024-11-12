1be92c72ac40c21b2e42ead1b6722b955d76caef
Daniel Montes <129787437+danielmontes23@users.noreply.github.com>
2024-11-12 23:05:36 +0000

    Added Euclidean Distance and tried to save the clusters

--- Homework 3/hw3.java
+++ Homework 3/hw3.java
@@ -29,4 +29,24 @@ public class Homework3 {
         saveClusters(data, assignments, k);
     }
 
+
+    private double euclideanDistance(double[] a, double[] b) {
+        double sum = 0;
+        for (int i = 0; i < a.length; i++) {
+            sum += Math.pow(a[i] - b[i], 2);
+        }
+        return Math.sqrt(sum);
+    }
+
+    private void saveClusters(List<double[]> data, List<Integer> assignments, int k) throws IOException {
+        for (int i = 0; i < k; i++) {
+            try (BufferedWriter writer = new BufferedWriter(new FileWriter("/workspaces/CSCD429/Homework 3/cluster_" + i + ".txt"))) {
+                for (int j = 0; j < data.size(); j++) {
+                    if (assignments.get(j) == i) {
+                        writer.write(Arrays.toString(data.get(j)).replaceAll("[\\[\\],]", "") + "\n");
+                    }
+                }
+            }
+        }
+    }
 }
